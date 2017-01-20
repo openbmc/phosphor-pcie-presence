4c95e4802afb7dc5c517f649c6d340b1e5d97068
Nan Li <william.bjlinan@hotmail.com>
2017-01-20 11:36:34 +0800

    Implement phosphor-pcie-presence with
    * Create initial bootstrap.sh, makefile, config.ac, etc
    * In main function, Use sd_event to receive the i/o interrupt
    * refactor pciedetector with sdbusplus
    * There are still lots of work to do
    
    Related to openbmc/openbmc#770
    
    Change-Id: I643340e17568179feadeacf19fef2527f13439eb
    Signed-off-by: Nan Li <william.bjlinan@hotmail.com>
    Signed-off-by: NanLi <william.bjlinan@hotmail.com>

---
+++ interface.hpp
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <string>
+#include <systemd/sd-event.h>
+#include <sdbusplus/server.hpp>
+
+namespace phosphor
+{
+namespace pcie
+{
+namespace presence
+{
+
+class SlotPresence
+{
+    public:
+        SlotPresence() = delete;
+        SlotPresence(const SlotPresence&) = delete;
+        SlotPresence(SlotPresence&&) = default;
+        SlotPresence operator=(const SlotPresence&) = delete;
+        SlotPresence& operator=(SlotPresence&&) = default;
+        ~SlotPresence() = default;
+
+static int ioHandler(sd_event_source*,
+                        int,
+                        uint32_t,
+                        void*);
+
+    private:
+        //SlotPresence
+        sdbusplus::bus::bus& bus;
+
+        sd_event* ioEvent;
+};
+
+} // namespace presence
+} // namespace pcie
+} // namespace phosphor
---
+++ pcie_presence_main.cpp
@@ -0,0 +1,67 @@
+#include <cstdlib>
+#include <iostream>
+#include <exception>
+#include <sdbusplus/bus.hpp>
+#include "config.h"
+#include <log.hpp>
+#include <systemd/sd-event.h>
+#include <sys/epoll.h>
+#include "interface.hpp"
+#include <sdbusplus/vtable.hpp>
+
+int main(int argc, char *argv[])
+{
+    std::string service, objpath;
+
+    if(argc <= 1)
+    {
+        std::cerr << "usage: " << argv[0]
+            << " <pcie-presence object path> "
+            << "e.g /xyz/openbmc_project/PATH/TO/gpioN" << std::endl;
+        return -1;
+    }
+
+    objpath = argv[1];
+
+    using namespace phosphor::logging;
+
+    phosphor::pcie::presence::SlotPresence SlotPresence();
+
+    auto bus = sdbusplus::bus::new_default();
+
+    sd_event* ioEvent = nullptr;
+    sd_event_source *eventSource = nullptr;
+
+    auto fd = epoll_create(1);
+
+    auto r = sd_event_default(&ioEvent);
+
+    if (r < 0) {
+        log<level::ERR>("pcie presence: obtain event loop  error",
+        entry("ERROR=%s", strerror(-r)));
+        goto finish;
+    }
+
+    bus.attach_event(ioEvent, SD_EVENT_PRIORITY_NORMAL);
+
+    if (r < 0) {
+        log<level::ERR>("pcie presence: attach system bus to event loop error",
+        entry("ERROR=%s", strerror(-r)));
+        goto finish;
+    }
+    sd_event_add_io(ioEvent,
+        &eventSource,
+        fd,
+        EPOLLIN,
+        phosphor::pcie::presence::SlotPresence::ioHandler, // SlotPresence
+        nullptr);
+
+    sd_event_loop(ioEvent);
+
+finish:
+    sd_event_source_unref(eventSource);
+//      bus.detach_event();
+    sd_event_unref(ioEvent);
+
+    return 0;
+}
